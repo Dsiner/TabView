e017a24e01be6ca0df176b326683093ac0163d76
D <s90789@outlook.com>
2017-05-30 00:01:18 +0800

    init

---
+++ app/src/main/java/com/d/tabview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.d.tabview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
