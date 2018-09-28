142fc41dd14bf64689a5e27533d73f4a80b8c3e4
D <s90789@outlook.com>
2018-09-28 15:20:01 +0800

    Update demo

--- app/src/main/java/com/d/tabview/MainActivity.java
+++ app/src/main/java/com/d/tabview/MainActivity.java
@@ -6,20 +6,36 @@ import android.widget.TextView;
 
 import com.d.lib.tabview.TabView;
 
-public class MainActivity extends AppCompatActivity implements TabView.OnTabSelectedListener {
-    private TextView tvTips;
+public class MainActivity extends AppCompatActivity {
+    private TextView[] tvTips = new TextView[3];
+    private TabView[] tabViews = new TabView[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvTips = (TextView) findViewById(R.id.tv_tips);
-        TabView tabView = (TabView) findViewById(R.id.tabv_tab);
-        tabView.setOnTabSelectedListener(this);
+        bindView();
+        init();
     }
 
-    @Override
-    public void onTabSelected(int position) {
-        tvTips.setText("onTab: " + (position + 1));
+    private void bindView() {
+        tvTips[0] = (TextView) findViewById(R.id.tv_tips0);
+        tvTips[1] = (TextView) findViewById(R.id.tv_tips1);
+        tvTips[2] = (TextView) findViewById(R.id.tv_tips2);
+        tabViews[0] = (TabView) findViewById(R.id.tabv_tab0);
+        tabViews[1] = (TabView) findViewById(R.id.tabv_tab1);
+        tabViews[2] = (TabView) findViewById(R.id.tabv_tab2);
+    }
+
+    private void init() {
+        for (int i = 0; i < tabViews.length; i++) {
+            final int finalI = i;
+            tabViews[finalI].setOnTabSelectedListener(new TabView.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(int index) {
+                    tvTips[finalI].setText("onTab: " + (index + 1));
+                }
+            });
+        }
     }
 }
